fee4c9ab6961c48a369ea7e6d7fc36c6ba52e907
VinokurovDmitriy <dmitvitvin@gmail.com>
2022-04-23 13:56:40 +0300

    add task27

---
+++ task27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа принимает на вход число и выводит на экран сумму цифр числа
+//452 -> 11
+//82 -> 10
+//9012 ->12
+
+int num1 = 452;
+int num2 = 82;
+int num3 = 9012;
+
+void printNumSum(int num){
+    int result = 0;
+    Console.Write($"{num} -> ");
+    while(num != 0){
+        result += num%10;
+        num /= 10;
+    }
+    Console.WriteLine(result);
+}
+
+printNumSum(num1);
+printNumSum(num2);
+printNumSum(num3);
